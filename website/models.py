74fc85dfcd117637affccf590491c5361e854715
sinqx <sinqxpls@gmail.com>
2023-11-29 17:27:07 +0600

    add surname

--- website/models.py
+++ website/models.py
@@ -8,28 +8,14 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(300), nullable=False)
     firstName = db.Column(db.String(30), nullable=False)
     secondName = db.Column(db.String(30), nullable=False)
+    surName = db.Column(db.String(30), nullable=False)
     department = db.Column(db.String(90))
     role = db.Column(db.String(20))
     room = db.relationship("Room", backref="user")
-    message = db.relationship("Message", backref="user")
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mainText = db.Column(db.String(650), nullable=False)
-    messageTheme = db.Column(db.String(50), nullable=False)
-    creationDate = db.Column(db.DateTime(timezone=True), nullable=False)
-    sentFrom = db.Column(db.Integer, db.ForeignKey("user.id"))
-    sentTo = db.Column(db.String(40), nullable=False)
-    sentToHead = db.Column(db.String(40), nullable=False)
-    userPosition = db.Column(db.String(40))
-    userInitials = db.Column(db.String(20), nullable=False)
-    isViewed = db.Column(db.Boolean, default=False)
-
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    roomNumber = db.Column(db.Integer, nullable=False)
+    roomName = db.Column(db.String(30), nullable=False)
     conferenceTitle = db.Column(db.String(60), nullable=False)
     startDate = db.Column(db.DateTime(timezone=True), nullable=False)
     endDate = db.Column(db.DateTime(timezone=True), nullable=False)
